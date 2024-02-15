1bbb0c08aff448c60056626eff0621454f8cc538
bharatm29 <bharatmhes@gmail.com>
2024-02-15 22:57:17 +0530

    connected with backend :(

--- src/components/CalenderEvents.tsx
+++ src/components/CalenderEvents.tsx
@@ -1,40 +1,20 @@
-/*
-const fetchLectures = async (date: string) => {
-    const res = await fetch(
-        "http://localhost:4000/timetable/date-wise/" + date
-    );
-
-    return res.json();
-};
-*/
-
 function getDate(): string {
     const date = new Date();
-    const weekday = new Intl.DateTimeFormat('en', { weekday: 'short' }).format(date);
-    const month = new Intl.DateTimeFormat('en', { month: 'long' }).format(date);
-    const day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
-    return (`${day} ${month}, ${weekday}`);
+    const weekday = new Intl.DateTimeFormat("en", { weekday: "short" }).format(
+        date
+    );
+    const month = new Intl.DateTimeFormat("en", { month: "long" }).format(date);
+    const day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(date);
+    return `${day} ${month}, ${weekday}`;
 }
 
-export default function CalenderEvents() {
-    /*
-    const [dateSelected, setDateSelected] = useState<string>(
-        new Date().toISOString().slice(0, 10)
-    );
-
-    const lecturesData = useQuery({
-        queryFn: () => fetchLectures(dateSelected),
-        //cached and fresh for 60 minutes
-        staleTime: 1000 * 60 * 60,
-        gcTime: 1000 * 60 * 60,
-        queryKey: [dateSelected],
-    });
-    */
-
-    return <div className="calender-container">
-        <div className="current-date-container">
-        {/*TODO::- add calendar icon here*/}
-            {getDate()}
+export default function CalenderEvents(/*{setCurrentDate}*/) {
+    return (
+        <div className="calender-container">
+            <div className="current-date-container">
+                {/*TODO::- add calendar icon here*/}
+                {getDate()}
+            </div>
         </div>
-    </div>;
+    );
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,43 @@
+import { useState } from "react";
 import CalenderEvents from "./CalenderEvents";
+import { useQuery } from "@tanstack/react-query";
+import Lecture from "./Lecture";
 
-export default function Home(){
+const fetchQuizes = async (date: string) => {
+    const res = await fetch(
+        "http://localhost:4000/quiz/get/" + date
+    );
+
+    return res.json();
+};
+
+const fetchLectures = async (date: string) => {
+    const res = await fetch(
+        "http://localhost:4000/timetable/date-wise/" + date
+    );
+
+    return res.json();
+};
+
+export default function Home() {
+    const [currentDate, setCurrentDate] = useState<string>(
+        new Date().toISOString().slice(0, 10)
+    );
+
+    const quizData = useQuery({
+        queryFn: () => fetchQuizes(currentDate),
+        staleTime: 1000 * 60 * 60,
+        gcTime: 1000 * 60 * 60,
+        queryKey: ["Quizes" + currentDate],
+    });
+
+    const lecturesData = useQuery({
+        queryFn: () => fetchLectures(currentDate),
+        staleTime: 1000 * 60 * 60,
+        gcTime: 1000 * 60 * 60,
+        queryKey: ["Lectures" + currentDate],
+    });
+
     return (
         <div className="home-container">
             <div className="heading-container">
@@ -14,12 +51,29 @@ export default function Home(){
             <div className="event-container">
                 <div className="assignment-test-container">
                     <h1>Assignment and Test</h1>
+                    <div className="assignment-test-list-container">
+                        {quizData.isSuccess && JSON.stringify(quizData.data)}
+                    </div>
                 </div>
                 <div className="classes-container">
                     <h1>Classes</h1>
+                    {lecturesData.isSuccess &&
+                        lecturesData?.data?.lectures?.map(
+                            (lecture: {
+                                _id: string;
+                                courseName: string;
+                                room: string;
+                                time: string;
+                                date: string;
+                            }) => {
+                                return (
+                                    <Lecture key={lecture._id} {...lecture} />
+                                );
+                            }
+                        )}
                 </div>
             </div>
-            <CalenderEvents />
+            <CalenderEvents /*setCurrentDate={setCurrentDate}*/ />
         </div>
     );
 }
