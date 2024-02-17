7fdc881c42b7da1b2c02e61a0c65bdb55b01ebd8
soham1011 <sohammarothi.10@gmail.com>
2024-02-17 15:58:19 +0530

    added the global css and the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import Routing from "./Routing";
+// import Routing from "./Routing";
 import Navpane from "./components/Navpane";
+import "./components/globals.css"
 
 export default function App() {
     return (
         <>
             <div id="app-container">
                 <Navpane />
-                <Routing />
+                {/* <Routing /> */}
             </div>
         </>
     );
--- src/components/Navpane.tsx
+++ src/components/Navpane.tsx
@@ -1,11 +1,71 @@
-import { NavLink } from "react-router-dom"
+// import { NavLink } from "react-router-dom"
+import './NavPane.css'
+import tasksvg from './svgs/task.svg' 
+import ranksvg from './svgs/rank.svg'
+import homesvg from './svgs/home.svg'
 
 export default function Navpane() {
     return (
-        <div className="navpane-container">
-            <h1>Symlearn</h1>
-            <NavLink to={"/"}>Home</NavLink>
-            <NavLink to={"/courses"}>Courses</NavLink>
+        <div className="lay-out">
+            
+            <div className='symlearn-box'>
+                <span className='symlearn'>Symlearn</span>
+            </div>
+            <div className='top-box'>
+            <div className='top-box-css1'>
+                        <img src={homesvg}></img>
+                        <span className='top-box-text'>Home</span>
+                </div>
+                <div className='top-box-css1'>
+                        <img src={tasksvg}></img>
+                        <span className='top-box-text'>tasks</span>
+                </div>
+                <div className='top-box-css1'>
+                        <img src={ranksvg}></img>
+                        <span className='top-box-text'>rankings</span>
+                </div>
+
+            </div>
+
+            <div className='huh'></div>
+
+            <div className='middle-box'>
+            <div className='top-box-css1'>
+                        <img src={tasksvg}></img>
+                        <span className='top-box-text'>demo1</span>
+                </div>
+                <div className='top-box-css1'>
+                        <img src={tasksvg}></img>
+                        <span className='top-box-text'>demo2</span>
+                </div>
+
+                <div className='top-box-css1'>
+                        <img src={tasksvg}></img>
+                        <span className='top-box-text'>demo3</span>
+                </div>
+
+                <div className='top-box-css1'>
+                        <img src={tasksvg}></img>
+                        <span className='top-box-text'>demo4</span>
+                </div>
+
+            </div>
+
+
+            <div className='bottom-box'>
+                <div className='bottom-box-individual'>
+                    <img src={tasksvg}></img>
+                    <span className='bottom-box-text'>info</span>
+                </div>
+                <div className='bottom-box-individual'>
+                    <img src={tasksvg}></img>
+                    <span className='bottom-box-text'>support</span>
+                </div>
+
+            </div>
+
+           
+            
         </div>
     );
 }
