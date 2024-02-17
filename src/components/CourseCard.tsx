eb0462cf73b9cfd1221b2a0d990c65ff7940fa8f
ArshhGill <arshh.gill@outlook.com>
2024-02-17 20:25:14 +0530

    commit with lazygit uwu

---
+++ src/components/CourseCard.tsx
@@ -0,0 +1,10 @@
+import "./CourseCard.css";
+
+export default function CourseCard({ data }) {
+    return <>
+        <div className="course-container">
+            <div className="course-top"></div>
+            <div className="course-bottom"></div>
+        </div>
+    </>
+}
